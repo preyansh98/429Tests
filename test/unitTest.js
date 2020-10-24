ed668d4537e2caf811ccbdd7ba81892cf4e92f45
preyansh98 <preyansh98@gmail.com>
2020-10-24 17:12:50 -0400

    Set up unit testing framework

---
+++ test/unitTest.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+describe('todos', () => {
+    describe('get all todos', () => {
+        it('should return a list of all the todos', ()=>{
+            assert.equal(1,1);
+        })
+    })
+})
